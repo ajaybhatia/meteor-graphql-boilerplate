56e706e44d8f42e0c37847ef4ad65ca7f7521708
Ajay Bhatia <prof.ajaybhatia@gmail.com>
2018-06-10 20:03:59 +0530

    apollo-client package reverted to verion 1.9.3 and client connection code also changed accordingly. This is done because accounts-password don't work in graphql with client with newer 2.x version.

--- imports/startup/client/index.jsx
+++ imports/startup/client/index.jsx
@@ -1,35 +1,20 @@
 /** @namespace Client */
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { ApolloClient, createNetworkInterface } from 'apollo-client';
+import { meteorClientConfig } from 'meteor/apollo';
 import { ApolloProvider } from 'react-apollo';
-import { ApolloLink, from } from 'apollo-link';
-import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import Routes from './routes.jsx';
 
-const httpLink = new HttpLink({
-  uri: Meteor.absoluteUrl("graphiql")
-});
+const networkInterface = createNetworkInterface({
+  uri: Meteor.absoluteUrl('graphql'),
+  useMeteorAccounts: true,
+  batchingInterface: true,
+  batchInterval: 10
+})
 
-const authLink = new ApolloLink((operation, forward) => {
-  const token = Accounts._storedLoginToken();
-  operation.setContext(() => ({
-    headers: {
-      "meteor-login-token": token
-    }
-  }));
-  return forward(operation);
-});
-
-const cache = new InMemoryCache();
-
-const client = new ApolloClient({
-  link: from([authLink, httpLink]),
-  cache
-});
+const client = new ApolloClient(meteorClientConfig({ networkInterface }));
 
 const App = () => (
   <ApolloProvider client={client}>
